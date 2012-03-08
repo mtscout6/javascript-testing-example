917a44d0c8329369f19fe63012fb622915b5c9ca
Matthew Smith <matthew.smith2@extendhealth.com>
2012-03-07 23:06:21 -0700

    Implemented adding tasks

--- src/todolist.js
+++ src/todolist.js
@@ -1,6 +1,17 @@
 define(function(){
-  var TodoListViewModel = function(){
-  };
+  var TodoItem = function(title){
+        var self = this;
+
+        self.title = title;
+      },
+      TodoListViewModel = function(){
+        var self = this;
+
+        self.tasks = [];
+        self.addTask = function(title){
+          self.tasks.push(new TodoItem(title));
+        };
+      };
 
   return TodoListViewModel;
 });
