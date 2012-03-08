63728734a2b9636ff7bb73b1dc782332db18c406
Matthew Smith <matthew.smith2@extendhealth.com>
2012-03-07 22:58:03 -0700

    Added a test

--- tests/todolist.spec.js
+++ tests/todolist.spec.js
@@ -1,6 +1,8 @@
 require(['../src/todolist'], function(TodoList){
   describe('Todo List Tests', function(){
-    var viewModel = new TodoList();
-    expect(viewModel).toBeDefined();
+    it('Should Initialize', function(){
+      var viewModel = new TodoList();
+      expect(viewModel).toBeDefined();
+    });
   });
 });
