cd097745305303c80a217ab3f57634737f74e69b
Matthew Smith <matthew.smith2@extendhealth.com>
2012-03-07 23:12:06 -0700

    Added test for added tasks in incomplete state

--- tests/todolist.spec.js
+++ tests/todolist.spec.js
@@ -14,5 +14,13 @@ require(['../src/todolist'], function(TodoList){
       expect(viewModel.tasks().length).toBe(1);
       expect(viewModel.tasks()[0].title()).toBe('SomeTask');
     });
+
+    it('Should leave added tasks in incomplete state', function(){
+      var viewModel = new TodoList();
+
+      viewModel.addTask('A Task');
+
+      expect(viewModel.tasks()[0].isComplete()).toBe(false);
+    });
   });
 });
