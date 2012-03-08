3885f6e800708fca766627870a53c8429fc2cec6
Matthew Smith <matthew.smith2@extendhealth.com>
2012-03-08 07:05:23 -0700

    Added tests for removing tasks

--- tests/todolist.spec.js
+++ tests/todolist.spec.js
@@ -1,4 +1,14 @@
 require(['../src/todolist'], function(TodoList){
+  var buildViewModelWithTaskItems = function(itemCount){
+    var viewModel = new TodoList();
+
+    for(var i = 0; i < itemCount; i++){
+      viewModel.addTask('Task ' + (i+1));
+    }
+
+    return viewModel;
+  };
+
   describe('Todo List Tests', function(){
     it('Should Initialize', function(){
       var viewModel = new TodoList();
@@ -17,10 +27,41 @@ require(['../src/todolist'], function(TodoList){
 
     it('Should leave added tasks in incomplete state', function(){
       var viewModel = new TodoList();
-
       viewModel.addTask('A Task');
-
       expect(viewModel.tasks()[0].isComplete()).toBe(false);
+    });
+
+    it('Should remove tasks', function(){
+      var viewModel = buildViewModelWithTaskItems(3),
+          tasks = [];
+
+      expect(viewModel.tasks().length).toBe(3);
+
+      for(var i = 0; i < viewModel.tasks().length; i++){
+        tasks.push(viewModel.tasks()[i]);
+      }
+
+      viewModel.removeTask(tasks[1]);
+      expect(viewModel.tasks().length).toBe(2);
+
+      viewModel.removeTask(tasks[0]);
+      expect(viewModel.tasks().length).toBe(1);
+
+      viewModel.removeTask(tasks[2]);
+      expect(viewModel.tasks().length).toBe(0);
+    });
+
+    it('Should not error when the same task is "removed" more than once', function(){
+      var viewModel = buildViewModelWithTaskItems(3),
+          task = viewModel.tasks()[1];
+
+      expect(viewModel.tasks().length).toBe(3);
+
+      viewModel.removeTask(task);
+      expect(viewModel.tasks().length).toBe(2);
+
+      viewModel.removeTask(task);
+      expect(viewModel.tasks().length).toBe(2);
     });
   });
 });
